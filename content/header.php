14d9d6ed8829d76b8b962aad635490c77a967470
grgojelavic <grgo.jelavic@aspira.hr>
2021-01-13 00:20:08 +0100

    03-html into php structure

--- content/header.php
+++ content/header.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=\, initial-scale=1.0">
     <title>Insurance</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="../css/main.css">
 </head>
 
 <body>
     <header>
         <div class="header-inner">
             <div class="logo">
-                <img src="./images/logo.png" alt="logo">
+                <img src="../images/logo.png" alt="logo">
             </div>
