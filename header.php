6250909104d61249140265cce6f4fac39cb04b20
grgojelavic <grgo.jelavic@aspira.hr>
2021-01-12 23:07:42 +0100

    02-footer and php structure

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Insurance</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+    <header>
+        <div class="header-inner">
+            <div class="logo">
+                <img src="./images/logo.png" alt="logo">
+            </div>
