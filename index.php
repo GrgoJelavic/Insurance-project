cc9172d0bec7e57543b4cbc3812d0493b51cb2f8
grgojelavic <grgo.jelavic@aspira.hr>
2021-01-19 09:39:50 +0100

    12-update policy  fix

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Insurance system</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/data.css">
+
     <script defer src="script.js"></script>
 
 
@@ -16,7 +15,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js"></script>  
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>  
 
-
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/data.css">
 
 </head>
 
@@ -43,21 +43,19 @@
 
         <main>
             <section>
-
-              <br /><br />
+                <br/>
                 <div class="container">
                 <h1>Live Search</h1>
-                <br/><br/>
+                <br/>
                 <label>Search Customers</label>
                 <div id="search_area">
                     <input type="text" name="customer_search" id="customer_search" class="form-control input-lg" autocomplete="off" placeholder="Search by any attribute"/>
                 </div>
-                <br />
-                <br />
+                <br/>
+                <br/>
                 <div id="search_Data"></div>
                 </div>
-
-                 
+                <br/><br/>
             </section>
         </main>
 
